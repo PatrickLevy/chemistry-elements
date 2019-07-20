3a1be519b0305a350c3bfa2ac36538b0f867c716
Patrick Levy <mrpatricklevy@gmail.com>
2019-07-20 09:11:36 -0500

    Restructure folders and files

---
+++ src/groups.js
@@ -0,0 +1,57 @@
+export const groups = {
+  alkaliMetal: {
+    id: 'alkaliMetal',
+    label: 'Alkali Metal',
+  },
+  alkalineEarthMetal: {
+    id: 'alkalineEarthMetal',
+    label: 'Alkaline Earth Metal',
+  },
+  transitionMetal: {
+    id: 'transitionMetal',
+    label: 'Transition Metal',
+  },
+  metalloid: {
+    id: 'metalloid',
+    label: 'Metalloid',
+  },
+  halogen: {
+    id: 'halogen',
+    label: 'Halogen',
+  },
+  nobleGas: {
+    id: 'nobleGas',
+    label: 'Noble Gas',
+  },
+  lanthanide: {
+    id: 'lanthanide',
+    label: 'Lanthanide',
+  },
+  actinide: {
+    id: 'actinide',
+    label: 'Actinide',
+  },
+  otherNonMetal: {
+    id: 'otherNonMetal',
+    label: 'Other Non-Metal',
+  },
+  otherMetal: {
+    id: 'otherMetal',
+    label: 'Other Metal',
+  },
+};
+
+export const metalTypes = {
+  metal: {
+    id: 'metal',
+    label: 'Metal',
+  },
+  nonMetal: {
+    id: 'nonMetal',
+    label: 'Non-Metal',
+  },
+  metalloid: {
+    id: 'metalloid',
+    label: 'Metalloid',
+  },
+};
